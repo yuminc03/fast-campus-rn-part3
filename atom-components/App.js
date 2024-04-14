1d8839924ac2baf6e0e90123c2d60b6344d96993
Yumin Chu <yuminc03@gmail.com>
2024-04-14 17:39:40 +0900

    Feat: Spacer 만들고 functional component로 바꿈

--- atom-components/App.js
+++ atom-components/App.js
@@ -2,6 +2,7 @@ import { StyleSheet, View } from 'react-native';
 import { Typography } from './src/components/Typography';
 import { LocalImage } from './src/components/LocalImage';
 import { RemoteImage } from './src/components/RemoteImage';
+import { Spacer } from './Spacer';
 
 export default function App() {
   return (
@@ -9,11 +10,24 @@ export default function App() {
       <Typography color='red' fontSize={24}>
         Open up App.js to start working on your app!
       </Typography>
-      <LocalImage 
-        localAsset={require('./assets/favicon.png')}
-        width={50}
-        height={50}
-      />
+      <Typography color='black' fontSize={16}>
+        TEXT
+      </Typography>
+      <Spacer space={40} />
+      <View style={{ flexDirection: 'row'}}>
+        <LocalImage 
+          localAsset={require('./assets/favicon.png')}
+          width={50}
+          height={50}
+        />
+        <Spacer horizontal={true} space={40} />
+        <LocalImage 
+          localAsset={require('./assets/favicon.png')}
+          width={50}
+          height={50}
+        />
+      </View>
+      <Spacer space={40} />
       <RemoteImage 
         url='https://i.imgur.com/TkIrScD.png'
         width={200}
