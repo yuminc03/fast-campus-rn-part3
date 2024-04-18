4aa5f7de07d98a0a4231541c5f472f6e845d847c
Yumin Chu <yuminc03@gmail.com>
2024-04-18 16:06:58 +0900

    Feat: 사용할 예정인 화면 기본 구현

---
+++ wallpaper/src/screen/FavofiteImageListScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Typography } from '../components/Typography';
+
+export const FavoriteImageListScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Typography>FAVORITE IMAGE</Typography>
+    </View>
+  );
+}
---
+++ wallpaper/src/screen/ImageListScreen.js
@@ -0,0 +1,11 @@
+import React from 'react-native';
+import { View } from 'react-native';
+import { Typography } from '../components/Typography';
+
+export const ImageListScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Typography fontSize={20}>IMAGE LIST</Typography>
+    </View>
+  );
+}
