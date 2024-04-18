d0876a7a8dff25ed4dbe8dd9f759027418fcdb96
Yumin Chu <yuminc03@gmail.com>
2024-04-19 08:57:49 +0900

    Feat: PhotoListItem 눌렀을 때 Detail화면으로 넘어가기

--- wallpaper/src/components/PhotoListItem.js
+++ wallpaper/src/components/PhotoListItem.js
@@ -3,11 +3,13 @@ import { useWindowDimensions } from "react-native";
 
 import { Button } from '../components/Button';
 import { RemoteImage } from './RemoteImage';
+import { useNavigation } from "@react-navigation/native";
 
 export const PhotoListItem = (props) => {
   const { width } = useWindowDimensions();
+  const navigation = useNavigation();
   const onPressItem = useCallback(() => {
-
+    navigation.navigate('ImageDetail', {url: props.url});
   }, []);
 
   return (
