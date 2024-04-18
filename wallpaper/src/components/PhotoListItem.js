54abec361c8fbf9394dae37db8384c5a87a0cf3e
Yumin Chu <yuminc03@gmail.com>
2024-04-18 16:54:59 +0900

    Feat: PhotoListItem - Button 수정하다가 중간 저장

---
+++ wallpaper/src/components/PhotoListItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useWindowDimensions } from "react-native";
+
+import { Button } from '../components/Button';
+
+export const PhotoListItem = (props) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Button>
+      
+    </Button>
+  )
+}
