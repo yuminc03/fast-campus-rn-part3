f7031fe41a2552d63fc43a0871b6617bc169d36d
Yumin Chu <yuminc03@gmail.com>
2024-04-18 10:32:23 +0900

    Feat: TabIcon.js - TabBarIcon에 Badge를 넣고 안 넣은 UI를 보여줌

---
+++ atom-components/src/components/TabIcon.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from 'react-native';
+import { Badge } from "./Badge";
+import { Icon } from "./Icons";
+
+export class TabIcon extends React.Component {
+  render() {
+    if (this.props.visibleBadge) {
+      return (
+        <View>
+          <Badge fontSize={10}>
+            <Icon name={this.props.iconName} size={20} color={'black'}/>
+          </Badge>
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        <Icon name={this.props.iconName} size={20} color={'black'}/>
+      </View>
+    );
+  }
+}
