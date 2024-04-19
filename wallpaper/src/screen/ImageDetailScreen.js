8d4c4e6df3cec69345b144c098c195e6a253be27
Yumin Chu <yuminc03@gmail.com>
2024-04-19 13:17:03 +0900

    Feat: 앨범 접근 권한 요청 팝업 띄우기

--- wallpaper/src/screen/ImageDetailScreen.js
+++ wallpaper/src/screen/ImageDetailScreen.js
@@ -1,17 +1,54 @@
 import React, { useCallback } from "react";
 import { View, useWindowDimensions } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
 
 import { Header } from "../components/Header/Header";
 import { RemoteImage } from "../components/RemoteImage";
+import { Button } from "../components/Button";
+import { Typography } from "../components/Typography";
+import { Icon } from "../components/Icons";
 
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation();
   const route = useRoute();
+  const { width } = useWindowDimensions();
   const onPressBack = useCallback(() => {
     navigation.goBack();
   }, []);
-  const { width } = useWindowDimensions();
+  const onPressDownload = useCallback(async () => {
+    const downloadResumable = FileSystem.createDownloadResumable(
+      route.params.url,
+      `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`
+    );
+
+    try {
+      const { uri } = await downloadResumable.downloadAsync();
+      console.log('Finished Downloading to ', uri);
+
+      const permissionResult = await MediaLibrary.getPermissionsAsync(true);
+      console.log('permissionResult: ', permissionResult);
+      if (permissionResult.status === 'denied') {
+        // 기기에서 못 쓰는 상황
+        return;
+      }
+
+      if (permissionResult.status === 'undetermined') {
+        const requestResult = await MediaLibrary.requestPermissionsAsync();
+        console.log(requestResult);
+        if (requestResult.status === 'denied') {
+          return;
+        }
+      }
+
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      const album = MediaLibrary.createAlbumAsync('TestFolder', asset, false);
+      console.log(album);
+    } catch(error) {
+      
+    }
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
@@ -24,6 +61,14 @@ export const ImageDetailScreen = (props) => {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <RemoteImage url={route.params.url} width={width} height={width * 1.5}/>
       </View>
+      <Button onPress={onPressDownload}>
+        <View style={{ paddingBottom: 24, backgroundColor: 'black' }}>
+          <View style={{ height: 52, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography color={'white'}>DOWNLOAD</Typography>
+            <Icon name='download' size={24} color='white'/>
+          </View>
+        </View>
+      </Button>
     </View>
   );
 }
