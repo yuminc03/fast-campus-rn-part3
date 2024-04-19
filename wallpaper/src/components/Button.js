5b9b21009e6fd5b1d05a6772d792a41830231d2a
Yumin Chu <yuminc03@gmail.com>
2024-04-19 09:17:56 +0900

    Fix: 이미지 padding이 적용되지 않은 문제 수정

--- wallpaper/src/components/Button.js
+++ wallpaper/src/components/Button.js
@@ -19,6 +19,8 @@ export const Button = (props) => {
     <Pressable 
       onPress={props.onPress} 
       hitSlop={props.hitSlop ?? { left: 0, right: 0, top: 0, bottom: 0 }}
+      paddingHorizontal={props.paddingHorizontal}
+      paddingVertical={props.paddingVertical}
     >
       {props.children}
     </Pressable>
