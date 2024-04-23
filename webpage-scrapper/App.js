612751916cadf4a6e4bbd635072c16bd79f1cb0e
Yumin Chu <yuminc03@gmail.com>
2024-04-23 16:54:02 +0900

    Feat: AsyncStorage에 데이터를 저장하는 기능 추가, useSetRecoilState 사용하기

--- webpage-scrapper/App.js
+++ webpage-scrapper/App.js
@@ -1,15 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { RootNavigation } from './src/navigations/RootNavigation';
 import { RecoilRoot } from 'recoil';
+
+import { RootNavigation } from './src/navigations/RootNavigation';
+import { RecoilCustomPersist } from './src/components/RecoilCustomPersist';
 
 export default function App() {
   return (
     <RecoilRoot>
       <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigation/>
-        </NavigationContainer>
+        <RecoilCustomPersist>
+          <NavigationContainer>
+            <RootNavigation/>
+          </NavigationContainer>
+        </RecoilCustomPersist>
       </SafeAreaProvider>
     </RecoilRoot>
   );
