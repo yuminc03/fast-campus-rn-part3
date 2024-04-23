72b969a4ad5f34089821dcbbba99182777e54298
Yumin Chu <yuminc03@gmail.com>
2024-04-23 13:25:10 +0900

    Feat: LottoNumberView - 애니메이션 넣기

--- lotto-number-get/src/components/LottoNumberView.js
+++ lotto-number-get/src/components/LottoNumberView.js
@@ -1,9 +1,11 @@
-import React, { useCallback } from "react";
-import { View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { View, Animated } from 'react-native';
 
 import { Typography } from "./Typography"; 
 
 export const LottoNumberView = (props) => {
+  const [ viewHeight, setViewHeight ] = useState(0);
+  const [ animatedValue ] = useState(new Animated.Value(1));
   const getNumberBackgroundColor = useCallback(() => {
     const randomNumber = Math.floor(Math.random() * 10) % 6;
     if (randomNumber === 0) { return 'pink'; } 
@@ -12,27 +14,53 @@ export const LottoNumberView = (props) => {
     if (randomNumber === 2) { return 'green'; }
     if (randomNumber === 2) { return 'purple'; }
     return 'black'
+  }, []);
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-viewHeight * 0.6, 0]
   });
 
+  useEffect(() => {
+    animatedValue.setValue(0);
+    Animated.timing(animatedValue, {
+      duration: 1000,
+      toValue: 1,
+    }).start();
+  }, [props.numbers]);
+
   return (
-    <View style={{ 
-      flex: 1, 
-      flexDirection: 'row', 
-      alignItems: 'center', 
-      justifyContent: 'space-between'
-    }}>
+    <View 
+      style={{ 
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-between',
+        overflow: 'hidden',
+      }}
+      onLayout={({ nativeEvent }) => {
+        console.log(nativeEvent.layout);
+        setViewHeight(nativeEvent.layout.height);
+      }}
+    >
       {props.numbers.map((item) => {
         return (
-          <View style={{ 
-            backgroundColor: getNumberBackgroundColor(item), 
-            width: 40, 
-            height: 40, 
-            borderRadius: 20, 
-            alignItems: 'center', 
-            justifyContent: 'center'
-          }}>
+          <Animated.View 
+            style={{ 
+              backgroundColor: getNumberBackgroundColor(item), 
+              width: 40, 
+              height: 40, 
+              borderRadius: 20, 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              transform: [
+                {
+                  translateY: translateY,
+                }
+              ]
+            }}
+          >
             <Typography fontSize={20} color='white'>{item}</Typography>
-          </View>
+          </Animated.View>
         );
       })}
     </View>
