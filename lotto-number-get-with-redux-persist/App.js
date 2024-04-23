c162ec332456a19e49538e4d699e44143dbc3123
Yumin Chu <yuminc03@gmail.com>
2024-04-23 13:59:26 +0900

    Feat: persist-reducer를 사용해서 lotto-number 데이터 유지하기

--- lotto-number-get-with-redux-persist/App.js
+++ lotto-number-get-with-redux-persist/App.js
@@ -1,17 +1,20 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { BottomTabNavigation } from './src/navigation/BottomTabNavigation';
-import store from './src/store/store';
+import { store, persistor } from './src/store/store';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <NavigationContainer>
-          <BottomTabNavigation/>
-        </NavigationContainer>
+        <PersistGate persistor={persistor}>
+          <NavigationContainer>
+            <BottomTabNavigation/>
+          </NavigationContainer>
+        </PersistGate>
       </Provider>
     </SafeAreaProvider>
   );
--- lotto-number-get-with-redux-persist/src/store/store.js
+++ lotto-number-get-with-redux-persist/src/store/store.js
@@ -1,10 +1,19 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { lottoNumberReducers } from "../reducers/lottoNumbers";
 import logger from "redux-logger";
+import persistReducer from "redux-persist/es/persistReducer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import hardSet from "redux-persist/es/stateReconciler/hardSet";
+
+import { lottoNumberReducers } from "../reducers/lottoNumbers";
+import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
   numbers: lottoNumberReducers
 });
-const store = createStore(rootReducer, applyMiddleware(logger));
-
-export default store;
+const persistedReducer = persistReducer({
+  key: 'root',
+  storage: AsyncStorage,
+  stateReconciler: hardSet,
+}, rootReducer);
+export const store = createStore(persistedReducer, applyMiddleware(logger));
+export const persistor = persistStore(store);
