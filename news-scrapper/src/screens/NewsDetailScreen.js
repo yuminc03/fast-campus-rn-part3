93a580df082ddcb18d0b48362be6f648ce656cb0
Yumin Chu <yuminc03@gmail.com>
2024-04-24 16:14:51 +0900

    Feat: react-native-webview 설치, NewsDetail 화면에 WebView로 뉴스 보여주기

--- news-scrapper/src/screens/NewsDetailScreen.js
+++ news-scrapper/src/screens/NewsDetailScreen.js
@@ -1,11 +1,14 @@
 import React, { useCallback } from 'react';
 import { View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import WebView from 'react-native-webview';
 
 import { Header } from '../components/Header/Header';
+import { Spacer } from '../components/Spacer';
 
 export const NewsDetailScreen = () => {
   const navigation = useNavigation();
+  const routes = useRoute();
   const onPress = useCallback(() => {
     navigation.goBack();
   }, []);
@@ -15,9 +18,16 @@ export const NewsDetailScreen = () => {
       <Header>
         <Header.Group>
           <Header.Icon iconName='arrow-back' onPress={onPress}/>
-          <Header.Title title='NEWS_DETAIL'/>
+          <Spacer space={12}/>
+          <View style={{ maxWidth: 200 }}>
+            <Header.Title title='NEWS_DETAIL'/>
+          </View>
         </Header.Group>
       </Header>
+      <WebView
+        style={{ flex: 1 }}
+        source={{ uri: routes.params.newsItem.link }}
+      />
     </View>
   );
 }
