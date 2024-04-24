b4d0bbb52b239d355097d82edfa73a3fe17a6827
Yumin Chu <yuminc03@gmail.com>
2024-04-24 15:12:11 +0900

    Feat: reducer, store 만들고 App.js에 연결

--- news-scrapper/App.js
+++ news-scrapper/App.js
@@ -1,14 +1,18 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
 
 import { RootNavigation } from './src/navigations/RootNavigation';
+import store from './src/store/store';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigation/>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootNavigation/>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
