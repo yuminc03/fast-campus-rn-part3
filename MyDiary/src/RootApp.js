375ead9bebc44b857a4af3b65def9d36d43e6734
Yumin Chu <yuminc03@gmail.com>
2024-04-30 23:05:13 +0900

    Feat: RootApp - App의 root가 될 화면

---
+++ MyDiary/src/RootApp.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+export const RootApp = () => {
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  );
+}
