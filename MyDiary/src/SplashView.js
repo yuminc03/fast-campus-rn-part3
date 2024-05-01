15ba47483cbad91953e4cb2efbb595e58ed4d974
Yumin Chu <yuminc03@gmail.com>
2024-05-01 20:35:21 +0900

    Feat: googleLogin으로 받은 사용자 정보 출력

--- MyDiary/src/SplashView.js
+++ MyDiary/src/SplashView.js
@@ -7,15 +7,20 @@ import { Typography } from "./components/Typography";
 
 export const SplashView = (props) => {
   const [showLoginButton, setShowLoginButton] = useState(false);
+
   const signinUserIdenfify = useCallback(async (idToken) => {
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     const result = await auth().signInWithCredential(googleCredential);
+    // 사용자 정보가 들어오게 되는
+    console.log(result);
   }, []);
+  
   const onPressGoogleLogin = useCallback(async () => {
     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     const {idToken} = await GoogleSignin.signIn();
     signinUserIdenfify(idToken);
   }, []);
+
   const userSilentLogin = useCallback(async () => {
     try {
       const {idToken} = await GoogleSignin.signInSilently();
