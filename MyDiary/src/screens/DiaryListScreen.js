26b6bc10325e07619e455bbe2f3ac4c15d029c7d
Yumin Chu <yuminc03@gmail.com>
2024-05-01 19:10:45 +0900

    Feat: vector-icons 설치, DiaryListScreen 화면 구성

--- MyDiary/src/screens/DiaryListScreen.js
+++ MyDiary/src/screens/DiaryListScreen.js
@@ -1,23 +1,115 @@
-import React, { useCallback } from "react";
-import { View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { FlatList, View, useWindowDimensions } from "react-native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "../components/Header/Header";
-import { useNavigation } from "@react-navigation/native";
+import { Button } from "../components/Button";
+import { Icon } from "../components/Icons";
+import { RemoteImage } from '../components/RemoteImage';
+import { Spacer } from "../components/Spacer";
+import { Typography } from "../components/Typography";
 
 export const DiaryListScreen = () => {
   const navigation = useNavigation();
+  const safeAreaInsets = useSafeAreaInsets();
+  const {width} = useWindowDimensions();
+
   const onPressSettings = useCallback(() => {
     navigation.navigate('Setting');
   }, []);
+  const onPressAdd = useCallback(() => {
+    navigation.navigate('AddDiary');
+  }, []);
+  const [data, setData] = useState([
+    {
+      id: 0,
+      title: 'TITLE_0',
+      content: 'CONTENT_0',
+      createdAt: '2022-01-01',
+      updatedAt: '2022-01-01',
+      imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+    },
+    {
+      id: 1,
+      title: 'TITLE_1',
+      content: 'CONTENT_1',
+      createdAt: '2022-01-01',
+      updatedAt: '2022-01-01',
+      imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+    },
+    {
+      id: 2,
+      title: 'TITLE_2',
+      content: 'CONTENT_2',
+      createdAt: '2022-01-01',
+      updatedAt: '2022-01-01',
+      imageUrl: 'https://docs.expo.dev/static/images/tutorial/background-image.png',
+    },
+  ])
   
   return (
     <View style={{ flex: 1 }}>
-      <Header>
-        <Header.Group>
-          <Header.Title title='Diary List'/>
-        </Header.Group>
-        <Header.Icon iconName='settings' onPress={onPressSettings}/>
-      </Header>
+      <View style={{ flex: 1 }}>
+        <Header>
+          <Header.Group>
+            <Header.Title title='Diary List'/>
+          </Header.Group>
+          <Header.Icon iconName='settings' onPress={onPressSettings}/>
+        </Header>
+
+        <FlatList
+          data={data}
+          contentContainerStyle={{
+            paddingHorizontal: 24,
+            paddingVertical: 32
+          }}
+          renderItem={({item}) => {
+            return (
+              <Button onPress={() => { 
+                navigation.navigate('DiaryDetail', {item})
+              }}>
+                <View style={{ paddingVertical: 12 }}>
+                  {item.imageUrl !== null && (
+                    <>
+                      <RemoteImage 
+                        url={item.imageUrl}
+                        width={width - 24 * 2}
+                        height={(width- 24 * 2) * 0.5}
+                        style={{borderRadius: 8}}
+                      />
+                      <Spacer space={4}/>
+                    </>
+                  )}
+                  <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <View>
+                      <Typography fontSize={18}>{item.title}</Typography>
+                      <Spacer space={4}/>
+                      <Typography fontSize={12}>{item.content}</Typography>
+                    </View>
+
+                    <Typography fontSize={12}>{item.updatedAt}</Typography>
+                  </View>
+                </View>
+              </Button>
+            );
+          }}
+        />
+      </View>
+      <View style={{ position: 'absolute', right: 12, bottom: safeAreaInsets.bottom + 24 }}>
+        <Button onPress={onPressAdd}>
+          <View style={{ 
+            width: 60, 
+            height: 60, 
+            borderRadius: 30, 
+            backgroundColor: 'black', 
+            alignItems: 'center', 
+            justifyContent: 'center'
+          }}>
+            <Icon name='add' color='white' size={30}/>
+          </View>
+        </Button>
+      </View>
     </View>
   );
 }
