5fed2a552ec22b229cccb701a9b21e9fa581c261
Yumin Chu <yuminc03@gmail.com>
2024-05-01 14:53:46 +0900

    Feat: 처음화면으로 spalsh 보여주고 2초 후 DiaryList 화면으로 전환

--- MyDiary/src/RootApp.js
+++ MyDiary/src/RootApp.js
@@ -2,8 +2,18 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { RootStackNavigation } from "./navigations/RootStackNavigation";
+import { SplashView } from "./SplashView";
 
 export const RootApp = () => {
+  const [initialized, setInitialized] = useState(false);
+  
+  if (!initialized) {
+    return (
+      <SplashView
+        onFinishLoad={() => setInitialized(true)}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <RootStackNavigation/>
---
+++ MyDiary/src/SplashView.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { View } from "react-native";
+
+import { Typography } from "./components/Typography";
+
+export const SplashView = (props) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      props.onFinishLoad();
+    }, 2000);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Typography fontSize={26}>
+        SPLASH
+      </Typography>
+    </View>
+  );
+}
