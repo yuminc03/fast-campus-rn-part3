7efbb791bc77a1b1a0d52d9bfa4956bca26c72c3
Yumin Chu <yuminc03@gmail.com>
2024-05-17 15:36:48 +0900

    Feat: MyPage 이미지 누르면 이동하는 FeedList 화면 UI 작업

--- anstagram/src/screens/FeedListScreen.tsx
+++ anstagram/src/screens/FeedListScreen.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 
 import { Header } from '../components/Header/Header';
+import { FeedListItem } from '../components/FeedListItem';
+import { Spacer } from '../components/Spacer';
+import { useRootNavigation, useRootRoute } from '../navigations/RootStackNavigation';
 
 export const FeedListScreen: React.FC = () => {
+  const route = useRootRoute<'FeedList'>();
+  const navigation = useRootNavigation<'FeedList'>();
+
   return (
     <View style={{flex: 1}}>
       <Header>
         <Header.Title title='FEED LIST' />
+        <Header.Icon iconName='close' onPress={() => {navigation.goBack()}}/>
       </Header>
+      
+      <FlatList 
+        data={route.params.list}
+        renderItem={({item}) => {
+          return (
+            <FeedListItem 
+              image={item.imageUrl}
+              comment={item.content}
+              isLiked={false}
+              likeCount={item.likeHistory.length}
+              writer={item.writer.name}
+              onPressFeed={() => {}}
+            />
+          )
+        }}
+        ItemSeparatorComponent={() => (
+          <Spacer space={24} />
+        )}
+      />
     </View>
   );
 }
