a5b34d41ee2799c34f43e596c9746f0ba6188ea0
Yumin Chu <yuminc03@gmail.com>
2024-05-20 12:59:11 +0900

    Feat: google-mobile-ads 설치, 테스트 광고 보여주기

--- anstagram/App.tsx
+++ anstagram/App.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import 'react-native-gesture-handler';
+import { BannerAd, BannerAdSize, MobileAds, TestIds } from 'react-native-google-mobile-ads';
 
 import { RootApp } from './src/RootApp';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
 
 GoogleSignin.configure();
+MobileAds().initialize().then((result) => {
+  console.log(result);
+});
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
         <RootApp />
+        <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.FULL_BANNER} />
       </Provider>
     </SafeAreaProvider>
   );
